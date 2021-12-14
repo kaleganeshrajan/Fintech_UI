c5c78bd0a9fcffdbfdd2d9c75cb0bf95c3e5039e
Shubham <ganesh@aiocdawacs.com>
2021-12-14 18:13:52 +0530

    company settings and make payment

---
+++ src/app/payment-settings/make-payment/make-payment.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { MakePaymentPage } from './make-payment.page';
+
+const routes: Routes = [
+	{
+		path: '',
+		component: MakePaymentPage
+	}
+];
+
+@NgModule({
+	imports: [
+		CommonModule,
+		FormsModule,
+		IonicModule,
+		RouterModule.forChild(routes),
+		ReactiveFormsModule
+	],
+	declarations: [MakePaymentPage]
+})
+export class MakePaymentPageModule { }
